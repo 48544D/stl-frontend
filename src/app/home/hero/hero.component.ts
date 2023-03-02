c539bc3bd711cad9518228ae1840ebbf56f027fa
48544D <haythamaaraba7@gmail.com>
2023-03-02 15:47:15 +0100

    dynamic hero title and text

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
   slides = [
-    { image: 'assets/images/home/carousel-1.png', text: 'Slide 1' },
-    { image: 'https://via.placeholder.com/800x400/0000FF/FFFFFF', text: 'Slide 2' },
-    { image: 'https://via.placeholder.com/800x400/FF0000/FFFFFF', text: 'Slide 3' }
+    { image: 'assets/images/home/carousel-1.png', title: 'Nous sommes les experts du transport routier', text: 'Le seul transporteur qui respecta rigoureusement les bonnes pratiques du transport routier' },
+    { image: 'https://via.placeholder.com/800x400/0000FF/FFFFFF', title: '' , text: 'Slide 2' },
+    { image: 'https://via.placeholder.com/800x400/FF0000/FFFFFF', title: '' ,text: 'Slide 3' }
   ];
 }
