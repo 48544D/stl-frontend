2a8716038f21995c4a7f0b20698d4aef4331550b
48544D <haythamaaraba7@gmail.com>
2023-03-08 09:32:20 +0100

    nos solutions html and css

--- src/app/home/solutions/solutions.component.ts
+++ src/app/home/solutions/solutions.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./solutions.component.scss']
 })
 export class SolutionsComponent {
+  activeTab: string = 'tab1';
 
+  openTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
